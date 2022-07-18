404631adee4ba94149f348584e6c1f5995cd1eb4
Gabriel Marques <bielmarques922@gmail.com>
2022-07-18 16:34:08 -0300

    Java Polimorfismo - Modulo 06 - Mais uma classe abstrata
    Modulo 06 - Mais uma classe abstrata

---
+++ 06-bytebank-herdado/src/Gerente.java
@@ -0,0 +1,8 @@
+
+public class Gerente extends FuncionarioAutenticavel {
+
+	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificação do GERENTE");
+		return super.getSalario();
+	}
+}
